c996704b498d06ed027b90f612486985e5354d78
pawel kreski <pawel.kreski@devbridge.com>
2021-09-26 20:15:52 +0200

    Implement Singleton pattern

---
+++ DesignPatterns/DesignPatterns/SynchronousLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SynchronousLogger
+{
+    private static SynchronousLogger sLogger;
+    private SynchronousLogger() { }
+
+    public static SynchronousLogger GetInstance()
+    {
+        if(sLogger == null)
+        {
+            sLogger = new SynchronousLogger();
+        }
+
+        return sLogger;
+    }
+
+    public void Log(string msg)
+    {
+        Console.WriteLine($"sLogger: {msg}");
+    }
+}
